27bd8fed29f2c08b2ae3c6da1abc7028cafe68d3
samwise43 <116317836+samwise43@users.noreply.github.com>
2023-03-03 10:37:29 -0600

    Add files via upload

---
+++ myproject/myapp/views.py
@@ -0,0 +1,32 @@
+from django.urls import reverse_lazy
+from django.views.generic import (
+    ListView,
+    CreateView,
+    UpdateView,
+)
+from .models import Card
+# Create your views here.
+
+
+class CardListView(ListView):
+    model = Card
+    queryset = Card.objects.all().order_by('box','-date_created')
+
+class CardCreateView(CreateView):
+    model = Card
+    fields = ['question', 'answer', 'box']
+    success_url = reverse_lazy('card-create')
+
+class CardUpdateView(CardCreateView, UpdateView):
+    success_url= reverse_lazy('card-list')
+
+class BoxView(CardListView):
+    template_name = 'myapp/box.html'
+
+    def get_queryset(self):
+        return Card.objects.filter(box=self.kwargs['box_num'])
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['box_number'] = self.kwargs['box_num']
+        return context
